48208ecab53d971850daaa09eb464cadd99b39a2
Bruno Langer <brunojoselanger@gmail.com>
2025-04-10 15:17:30 -0300

    better word pairs list

--- src/assets/list.js
+++ src/assets/list.js
@@ -1,18 +1,19 @@
 const wikiDailyPairs = {
-  "2025-04-10": ["Brasil", "Piracicaba"],
-  "2025-04-11": ["Café", "Starbucks"],
-  "2025-04-12": ["Apple Inc.", "iPhone"],
-  "2025-04-13": ["Python (linguagem de programação)", "ChatGPT"],
-  "2025-04-14": ["Futebol", "Copa do Mundo FIFA"],
-  "2025-04-15": ["YouTube", "PewDiePie"],
-  "2025-04-16": ["New York City", "Empire State Building"],
-  "2025-04-17": ["Tesla, Inc.", "Elon Musk"],
-  "2025-04-18": ["Matemática", "Teorema de Pitágoras"],
-  "2025-04-19": ["Inteligência artificial", "Alan Turing"],
-  "2025-04-20": ["Música", "Guitarra elétrica"],
-  "2025-04-21": ["Segunda Guerra Mundial", "Adolf Hitler"],
-  "2025-04-22": ["Linguagem de programação C", "UNIX"],
-  "2025-04-23": ["Impressionismo", "Claude Monet"]
+  "2025-04-10": ["Buraco Negro", "Pizza"],
+  "2025-04-11": ["Vladimir Lenin", "TikTok"],
+  "2025-04-12": ["Origami", "Teorema de Fermat"],
+  "2025-04-13": ["Candomblé", "Blockchain"],
+  "2025-04-14": ["Monte Everest", "Mona Lisa"],
+  "2025-04-15": ["Guerra Fria", "Dragon Ball Z"],
+  "2025-04-16": ["Mahatma Gandhi", "Netflix"],
+  "2025-04-17": ["Baleia Azul", "Teclado Mecânico"],
+  "2025-04-18": ["Ópera", "Sistema Operacional Linux"],
+  "2025-04-19": ["Cerveja", "Guerra do Vietnã"],
+  "2025-04-20": ["Vincent van Gogh", "Plutônio"],
+  "2025-04-21": ["Rock and Roll", "Imperador Romano"],
+  "2025-04-22": ["Super Mario", "Sigmund Freud"],
+  "2025-04-23": ["Taj Mahal", "Física Quântica"]
 };
+
 
 export { wikiDailyPairs };
